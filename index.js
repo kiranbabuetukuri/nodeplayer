86b1dfbbd19ec90c89da75dbf97e99dd580d9ed2
kiranbabuetukuri <kiranbabu.etukuri@gmail.com>
2022-01-11 21:30:50 +0530

    1

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors())
 const log=console.log
 const CONNECTION_URL="mongodb+srv://admin:password654@users.bvdv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-// const CONsNECTION_URL="mongodb://127.0.0.1:27017/player"
+ //const CONsNECTION_URL="mongodb://127.0.0.1:27017/player"
 const PORT=8080// process.env.PORT || 5000
 
 app.use("/posts",postRoutes)
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>app.listen(PORT,()=>log(`Server running on port ${PORT}`))).catch(error=>{
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>app.listen(PORT,()=>log(`Server running on port d ${PORT}`))).catch(error=>{
 log ("unable to connect mongodb url"+CONNECTION_URL)
     log(error)
 })
